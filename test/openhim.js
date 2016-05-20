8780162073873d7713a07a16e67b67a19dcefd6d
Ryan Crichton <ryan@jembi.org>
2016-05-20 12:01:51 +0200

    Cleanup and standarise code.

--- test/openhim.js
+++ test/openhim.js
@@ -1,65 +1,63 @@
 'use strict'
+/* global describe it beforeEach */
 
 const _ = require('lodash')
-const assert = require('assert');
-const nock = require('nock');
+const assert = require('assert')
+const nock = require('nock')
 
 const OpenHIM = require('../lib/openhim')
 
-describe('Register Channel: ', function(){
-  var checkValidRequest = function(body){
-    return body.urlPattern && body.name && body.routes[0].name
-      && body.routes[0].host && body.routes[0].port? true : false;
+describe('Register Channel: ', function () {
+  var checkValidRequest = function (body) {
+    return body.urlPattern && body.name && body.routes[0].name && body.routes[0].host && body.routes[0].port
   }
 
-  beforeEach(function() {
+  beforeEach(function () {
     // mock OpenHim server reponses
-    nock("http://localhost:8080")
-      .get("/authenticate/root@openhim.org")
+    nock('http://localhost:8080')
+      .get('/authenticate/root@openhim.org')
       .reply(200, {
-        "salt": "test-salt",
-        "ts": "test-ts"
+        'salt': 'test-salt',
+        'ts': 'test-ts'
       })
-      .post("/channels", function(body){
-         return checkValidRequest(body);
+      .post('/channels', function (body) {
+        return checkValidRequest(body)
       })
       .reply(201)
-      .post("/channels", function(body){
+      .post('/channels', function (body) {
         // only want to accept invalid requests here
-        return checkValidRequest(body)? false : true;
+        return !checkValidRequest(body)
       })
-      .reply(400);
-	});
+      .reply(400)
+  })
 
-  var testChannels = _.cloneDeep(require("../resources/test_channels.json"));
-  var validChannelConfig = testChannels.valid;
-  var invalidChannelConfig = testChannels.invalid;
+  var testChannels = _.cloneDeep(require('../resources/test_channels.json'))
+  var validChannelConfig = testChannels.valid
+  var invalidChannelConfig = testChannels.invalid
 
   let apiConfig = {
-    username: "root@openhim.org",
-    password: "password",
-    url: "https://localhost:8080"
+    username: 'root@openhim.org',
+    password: 'password',
+    url: 'https://localhost:8080'
   }
 
   let openhim = OpenHIM(apiConfig)
 
-  it('should register openhim channel successfully', function(done){
-    openhim.registerChannel(validChannelConfig, function(err, result){
-      if(err) {
-        return done(err);
+  it('should register openhim channel successfully', function (done) {
+    openhim.registerChannel(validChannelConfig, function (err, result) {
+      if (err) {
+        return done(err)
       }
-      assert(result.statusCode==201, "Channel created successfully");
-      done();
-    });
-  });
+      assert(result.statusCode === 201, 'Channel created successfully')
+      done()
+    })
+  })
 
-  it('should fail to register openhim channel', function(done){
-    openhim.registerChannel(invalidChannelConfig, function(err, result){
-      if(err) {
-        return done(err);
-      }
-      assert(result.statusCode==400, "Failed to create channel");
-      done();
-    });
-  });
-});
+  it('should fail to register openhim channel', function (done) {
+    openhim.registerChannel(invalidChannelConfig, function (err, result) {
+      assert(err, 'Error is ok')
+      assert(result.statusCode === 400, 'Failed to create channel')
+      done()
+    })
+  })
+})
--- test/openinfoman.js
+++ test/openinfoman.js
@@ -1,69 +1,70 @@
 'use strict'
+/* global describe it beforeEach */
 
-const assert = require('assert');
-const nock = require('nock');
+const assert = require('assert')
+const nock = require('nock')
 
-const OpenInfoMan = require('../lib/openinfoman');
+const OpenInfoMan = require('../lib/openinfoman')
 const openinfoman = OpenInfoMan('http://localhost:8984')
 
 var expectedResponse = {
-  "providers": {
-      "careServicesRequest": "http://localhost:8984/CSD/csr/providers/careServicesRequest",
-      "careServicesRequests": {
-          "urn:ihe:iti:csd:2014:stored-function:facility-search": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:facility-search",
-          "urn:ihe:iti:csd:2014:adhoc": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:adhoc",
-          "urn:ihe:iti:csd:2014:stored-function:service-search": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:service-search",
-          "urn:ihe:iti:csd:2014:stored-function:organization-search": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:organization-search",
-          "urn:ihe:iti:csd:2014:stored-function:provider-search": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:provider-search",
-          "urn:openhie.org:openinfoman:modtimes": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:modtimes",
-          "urn:openhie.org:openinfoman:service_create": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:service_create",
-          "urn:openhie.org:openinfoman:mark_duplicate": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:mark_duplicate",
-          "urn:openhie.org:openinfoman:simple_merge": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:simple_merge",
-          "urn:openhie.org:openinfoman:mark_potential_duplicate": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:mark_potential_duplicate",
-          "urn:openhie.org:openinfoman:delete_potential_duplicate": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:delete_potential_duplicate",
-          "urn:openhie.org:openinfoman:organization_create": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:organization_create",
-          "urn:openhie.org:openinfoman:provider_create": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:provider_create",
-          "urn:openhie.org:openinfoman:facility_create": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:facility_create",
-          "urn:openhie.org:openinfoman:delete_duplicate": "http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:delete_duplicate"
-      }
+  'providers': {
+    'careServicesRequest': 'http://localhost:8984/CSD/csr/providers/careServicesRequest',
+    'careServicesRequests': {
+      'urn:ihe:iti:csd:2014:stored-function:facility-search': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:facility-search',
+      'urn:ihe:iti:csd:2014:adhoc': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:adhoc',
+      'urn:ihe:iti:csd:2014:stored-function:service-search': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:service-search',
+      'urn:ihe:iti:csd:2014:stored-function:organization-search': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:organization-search',
+      'urn:ihe:iti:csd:2014:stored-function:provider-search': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:provider-search',
+      'urn:openhie.org:openinfoman:modtimes': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:modtimes',
+      'urn:openhie.org:openinfoman:service_create': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:service_create',
+      'urn:openhie.org:openinfoman:mark_duplicate': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:mark_duplicate',
+      'urn:openhie.org:openinfoman:simple_merge': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:simple_merge',
+      'urn:openhie.org:openinfoman:mark_potential_duplicate': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:mark_potential_duplicate',
+      'urn:openhie.org:openinfoman:delete_potential_duplicate': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:delete_potential_duplicate',
+      'urn:openhie.org:openinfoman:organization_create': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:organization_create',
+      'urn:openhie.org:openinfoman:provider_create': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:provider_create',
+      'urn:openhie.org:openinfoman:facility_create': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:facility_create',
+      'urn:openhie.org:openinfoman:delete_duplicate': 'http://localhost:8984/CSD/csr/providers/careServicesRequest/urn:openhie.org:openinfoman:delete_duplicate'
+    }
   },
-  "RapidProContacts": {
-      "careServicesRequest": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest",
-      "careServicesRequests": {
-          "urn:ihe:iti:csd:2014:stored-function:facility-search": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:facility-search",
-          "urn:ihe:iti:csd:2014:adhoc": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:adhoc",
-          "urn:ihe:iti:csd:2014:stored-function:service-search": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:service-search",
-          "urn:ihe:iti:csd:2014:stored-function:organization-search": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:organization-search",
-          "urn:ihe:iti:csd:2014:stored-function:provider-search": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:provider-search",
-          "urn:openhie.org:openinfoman:modtimes": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:modtimes",
-          "urn:openhie.org:openinfoman:service_create": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:service_create",
-          "urn:openhie.org:openinfoman:mark_duplicate": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:mark_duplicate",
-          "urn:openhie.org:openinfoman:simple_merge": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:simple_merge",
-          "urn:openhie.org:openinfoman:mark_potential_duplicate": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:mark_potential_duplicate",
-          "urn:openhie.org:openinfoman:delete_potential_duplicate": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:delete_potential_duplicate",
-          "urn:openhie.org:openinfoman:organization_create": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:organization_create",
-          "urn:openhie.org:openinfoman:provider_create": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:provider_create",
-          "urn:openhie.org:openinfoman:facility_create": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:facility_create",
-          "urn:openhie.org:openinfoman:delete_duplicate": "http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:delete_duplicate"
-      }
+  'RapidProContacts': {
+    'careServicesRequest': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest',
+    'careServicesRequests': {
+      'urn:ihe:iti:csd:2014:stored-function:facility-search': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:facility-search',
+      'urn:ihe:iti:csd:2014:adhoc': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:adhoc',
+      'urn:ihe:iti:csd:2014:stored-function:service-search': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:service-search',
+      'urn:ihe:iti:csd:2014:stored-function:organization-search': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:organization-search',
+      'urn:ihe:iti:csd:2014:stored-function:provider-search': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:provider-search',
+      'urn:openhie.org:openinfoman:modtimes': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:modtimes',
+      'urn:openhie.org:openinfoman:service_create': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:service_create',
+      'urn:openhie.org:openinfoman:mark_duplicate': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:mark_duplicate',
+      'urn:openhie.org:openinfoman:simple_merge': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:simple_merge',
+      'urn:openhie.org:openinfoman:mark_potential_duplicate': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:mark_potential_duplicate',
+      'urn:openhie.org:openinfoman:delete_potential_duplicate': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:delete_potential_duplicate',
+      'urn:openhie.org:openinfoman:organization_create': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:organization_create',
+      'urn:openhie.org:openinfoman:provider_create': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:provider_create',
+      'urn:openhie.org:openinfoman:facility_create': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:facility_create',
+      'urn:openhie.org:openinfoman:delete_duplicate': 'http://localhost:8984/CSD/csr/RapidProContacts/careServicesRequest/urn:openhie.org:openinfoman:delete_duplicate'
+    }
   }
-};
+}
 
-describe('Get documents', function() {
-  beforeEach(function() {
-    nock("http://localhost:8984")
-      .get("/CSD/documents.json")
-      .reply(200, expectedResponse);
-	});
+describe('Get documents', function () {
+  beforeEach(function () {
+    nock('http://localhost:8984')
+      .get('/CSD/documents.json')
+      .reply(200, expectedResponse)
+  })
 
-  it('should convert openinfoman GET result to array of documents', function(done) {
-  	openinfoman.getDocuments(function(err, result) {
-      if(err) {
-        return done(err);
+  it('should convert openinfoman GET result to array of documents', function (done) {
+    openinfoman.getDocuments(function (err, result) {
+      if (err) {
+        return done(err)
       }
-      assert.equal(result[0], 'providers', "Correct result array");
-      assert.equal(result[1], 'RapidProContacts', "Correct result array");
-      done();
-    });
-  });
-});
+      assert.equal(result[0], 'providers', 'Correct result array')
+      assert.equal(result[1], 'RapidProContacts', 'Correct result array')
+      done()
+    })
+  })
+})
